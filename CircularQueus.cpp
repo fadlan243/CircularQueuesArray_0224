775b268289c8956f997cb96a658cea5a6b5135e4
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-10 11:37:51 +0700

    jika elemen yang di hapus berada di posisi terakhir array, kembali ke awal

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -53,4 +53,12 @@ public :
         FRONT = -1;
         REAR = -1;
     }
-    
+    else {
+        // Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal
+        if (FRONT == max - 1)
+            FRONT = 0;
+        else    
+            FRONT = FRONT + 1; 
+    }
+};
+
