9cd4340276d92d3e2fa2f4b65fe95c9b6491d326
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-10 11:40:51 +0700

    iterasi dari awal array hingga rear

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -85,4 +85,15 @@ void display() {
                 FRONT_position++;
             }
 
-            
+            FRONT_position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
